1ce006794f892a2017556ed73e8214067ef2b956
Ruben <ruben.keller@student.htldornbirn.at>
2023-03-21 10:20:18 +0100

    Rubens Teil
    Pinwand & Menu eingefügt

--- Cafeteria.js
+++ Cafeteria.js
@@ -3,3 +3,10 @@ const colorPicker = document.getElementById('color-picker');
 colorPicker.addEventListener('input', function() {
   document.documentElement.style.setProperty('--output-color', this.value);
 });
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', function() {
+  menu.classList.toggle('show');
+});

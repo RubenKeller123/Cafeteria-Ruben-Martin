a692a5d279cdd5d4442ff19a5575df1bcc0f05a1
Ruben <ruben.keller@student.htldornbirn.at>
2023-04-17 13:17:48 +0200

    ganz fertig

--- Cafeteria.js
+++ Cafeteria.js
@@ -10,3 +10,48 @@ const menu = document.querySelector('.menu');
 menuBtn.addEventListener('click', function() {
   menu.classList.toggle('show');
 });
+
+var HomeDiv = document.getElementById("Home");
+var ButtonBack = document.getElementById("buttonBack");
+var MacherDiv = document.getElementById("Macher");
+var ContactDiv = document.getElementById("ContactUs")
+var ButtonMacher = document.getElementById("nr2");
+var ButtonContact = document.getElementById("nr3")
+
+ButtonBack.addEventListener("click", function() {
+  HomeDiv.style.display = "block";
+  MacherDiv.style.display = "none";
+  ContactDiv.style.display = "none";
+});
+ButtonMacher.addEventListener("click", function() {
+  MacherDiv.style.display = "block";
+  HomeDiv.style.display = "none";
+  ContactDiv.style.display = "none";
+});
+
+ButtonContact.addEventListener("click", function() {
+  MacherDiv.style.display = "none";
+  HomeDiv.style.display = "none";
+  ContactDiv.style.display = "block";
+});
+
+
+
+
+
+window.onload = function() {
+  // Get the overlay and popup elements
+  var overlay = document.querySelector('.overlay');
+  var popup = document.querySelector('.popup');
+
+  // Show the popup
+  overlay.style.display = 'flex';
+  popup.style.display = 'block';
+
+  // Add an event listener to the close button
+  var closeBtn = document.getElementById('closeBtn');
+  closeBtn.addEventListener('click', function() {
+      overlay.style.display = 'none';
+      popup.style.display = 'none';
+  });
+};
